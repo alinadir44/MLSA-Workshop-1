d6e474e2f473c5eaedbad0f2263ed10417e702f1
Daniyal Saeed Dani <62589953+Dani-Bhai@users.noreply.github.com>
2023-11-15 07:23:04 +0000

    Pending changes exported from your codespace

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ import resume from "../resume.pdf";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/portfolio.jpg";
 
 const imageAltText =
   "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
